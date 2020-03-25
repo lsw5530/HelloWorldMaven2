dfe9a0a2631298bc55e926c7c6130c90dccee64c
lsw5530 <lsw5530@gmail.com>
2020-03-25 15:38:48 +0800

    测试tomcat和maven热部署。

--- src/main/java/com/sikiedu/web/TestServlet.java
+++ src/main/java/com/sikiedu/web/TestServlet.java
@@ -26,7 +26,7 @@ public class TestServlet extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		response.getWriter().append(" Hello World Maven 123123");
+		response.getWriter().append(" Hello World Maven 666");
 	}
 
 	/**
